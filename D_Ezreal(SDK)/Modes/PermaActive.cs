4d6addd5d2d6df089ecaba07816fb09c858f4e54
diabaths <diabatiis@gmail.com>
2016-02-21 03:35:15 +0200

    i more

--- D_Ezreal(SDK)/Modes/PermaActive.cs
+++ D_Ezreal(SDK)/Modes/PermaActive.cs
@@ -86,7 +86,7 @@ namespace D_Ezreal_SDK_.Modes
                 }
             }
 
-            if (R.IsReady())
+            if (R.IsReady() && Settings.UseRM && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithR(true)))
             {
                 var target = Variables.TargetSelector.GetTarget(1500);
                 if (target != null)
@@ -104,24 +104,19 @@ namespace D_Ezreal_SDK_.Modes
                     if (prediction.Hitchance >= HitChance.High)
                     {
                         if (Q.IsReady() && W.IsReady() && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithQW(true))
-                               && target.IsValidTarget(Q.Range))
-                            return;
+                            && target.IsValidTarget(Q.Range)) return;
                         if (Q.IsReady() && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithQ(true))
-                            && target.IsValidTarget(Q.Range))
-                            return;
+                            && target.IsValidTarget(Q.Range)) return;
                         if (W.IsReady() && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithW(true))
-                            && target.IsValidTarget(W.Range))
-                            return;
+                            && target.IsValidTarget(W.Range)) return;
                         if (Q.IsReady() && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithQAuto(true))
-                           && target.IsValidTarget(Q.Range))
-                            return;
+                            && target.IsValidTarget(Q.Range)) return;
                         if (W.IsReady() && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithWAuto(true))
-                            && target.IsValidTarget(W.Range))
-                            return;
+                            && target.IsValidTarget(W.Range)) return;
                         if (target.DistanceToPlayer() < target.GetRealAutoAttackRange()
-                            && target.Health <= GameObjects.Player.GetAutoAttackDamage(target))
-                            return;
-                        if (Environment.TickCount - Modes.Combo.castR > 500 && Settings.UseRM && GameObjects.EnemyHeroes.Any(x => x.IsKillableWithR(true)) && target.DistanceToPlayer() > Config.Modes.Combo.Minrange)
+                            && target.Health <= GameObjects.Player.GetAutoAttackDamage(target)) return;
+                        if (Environment.TickCount - Modes.Combo.castR > 500
+                            && target.DistanceToPlayer() > Config.Modes.Combo.Minrange)
                         {
                             R.Cast(prediction.CastPosition);
                         }
